cd4b2fb50a0165ce73edb0ff31c3965cf533218d
Emanuele Bartolesi <bartolesiemanuele@hotmail.com>
2020-11-06 22:20:52 +0100

    first commit

---
+++ src/BlazorTeamsTabExample/Models/Group.cs
@@ -0,0 +1,83 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorTeamsTabExample.Models
+{
+    public class Group
+    {
+        [JsonProperty("mail")]
+        public string Mail { get; set; }
+
+        [JsonProperty("onPremisesSyncEnabled")]
+        public bool? OnPremisesSyncEnabled { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("createdDateTime")]
+        public string CreatedDateTime { get; set; }
+
+        [JsonProperty("classification")]
+        public object Classification { get; set; }
+
+        [JsonProperty("deletedDateTime")]
+        public object DeletedDateTime { get; set; }
+
+        [JsonProperty("groupTypes")]
+        public string[] GroupTypes { get; set; }
+
+        [JsonProperty("displayName")]
+        public string DisplayName { get; set; }
+
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("membershipRuleProcessingState")]
+        public string MembershipRuleProcessingState { get; set; }
+
+        [JsonProperty("mailNickname")]
+        public string MailNickname { get; set; }
+
+        [JsonProperty("mailEnabled")]
+        public bool MailEnabled { get; set; }
+
+        [JsonProperty("membershipRule")]
+        public object MembershipRule { get; set; }
+
+        [JsonProperty("onPremisesProvisioningErrors")]
+        public object[] OnPremisesProvisioningErrors { get; set; }
+
+        [JsonProperty("onPremisesLastSyncDateTime")]
+        public string OnPremisesLastSyncDateTime { get; set; }
+
+        [JsonProperty("onPremisesSecurityIdentifier")]
+        public string OnPremisesSecurityIdentifier { get; set; }
+
+        [JsonProperty("resourceBehaviorOptions")]
+        public string[] ResourceBehaviorOptions { get; set; }
+
+        [JsonProperty("proxyAddresses")]
+        public string[] ProxyAddresses { get; set; }
+
+        [JsonProperty("preferredLanguage")]
+        public object PreferredLanguage { get; set; }
+
+        [JsonProperty("renewedDateTime")]
+        public string RenewedDateTime { get; set; }
+
+        [JsonProperty("securityEnabled")]
+        public bool SecurityEnabled { get; set; }
+
+        [JsonProperty("resourceProvisioningOptions")]
+        public object[] ResourceProvisioningOptions { get; set; }
+
+        [JsonProperty("theme")]
+        public object Theme { get; set; }
+
+        [JsonProperty("visibility")]
+        public string Visibility { get; set; }
+    }
+}
---
+++ src/BlazorTeamsTabExample/Services/GraphService.cs
@@ -0,0 +1,62 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace BlazorTeamsTabExample.Services
+{
+    public class GraphService
+    {
+        public async Task<String> GetMeWithHttpClient(string token)
+        {
+            var _httpclient = new HttpClient();
+            _httpclient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("bearer", token);
+            var response = await _httpclient.GetAsync("https://graph.microsoft.com/v1.0/me/");
+
+            return response.Content.ReadAsStringAsync().Result;
+        }
+
+        public async Task<List<T>> GetAllEntities<T>(string entity, string token)
+        {
+            var entities = new List<T>();
+
+            var query = $"https://graph.microsoft.com/v1.0/{entity}";
+
+            var hasNextPage = true;
+
+            while (hasNextPage)
+            {
+                var _httpclient = new HttpClient();
+
+                var request = new HttpRequestMessage(HttpMethod.Get, query);
+                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+                try
+                {
+                    var response = await _httpclient.SendAsync(request);
+                    var json = await response.Content.ReadAsStringAsync();
+                    var jObject = JObject.Parse(json);
+                    var jObjects = (JArray)jObject["value"];
+
+                    entities.AddRange(jObjects.ToObject<List<T>>());
+
+                    query = (string)jObject["@odata.nextLink"];
+
+                    if (string.IsNullOrEmpty(query))
+                    {
+                        hasNextPage = false;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    hasNextPage = false;
+                }
+            }
+
+            return entities;
+        }
+    }
+}
